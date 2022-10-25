8b9f8bac482c6f6fc7d3b0fab76ccfd2dc431f04
polarisid <danielcarvalhorst@gmail.com>
2022-10-24 22:39:45 -0300

    First Commit

---
+++ projeto_1/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Lista de Compras</h1>
+        <ul>
+          <li>Pão</li>
+          <li>Banana</li>
+          <li>Milk Shake de Doce de Leite</li>
+          <li>Nutella</li>
+          <li>Sorvete</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
